cce70a598509e17219ed7333a1eca74efcf78bfe
Damian <d.chalupnik@hotmail.ocm>
2025-05-16 22:57:31 +0200

    Add icon4

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,12 @@ import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
 
 
-st.set_page_config(
-    page_title="Moltress",
-    page_icon="favicon.ico",
-    layout="wide",
-)
-
+# st.set_page_config(
+#     page_title="Moltress",
+#     page_icon="favicon.ico",
+#     layout="wide",
+# )
+st.logo("logo.png")
 st.set_page_config(layout="wide")
 nav = get_nav_from_toml(".streamlit/pages.toml")
 pg = st.navigation(nav)
