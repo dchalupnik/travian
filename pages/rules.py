820a1435826046fbdf41b24b2995c30a89d39b31
Damian <d.chalupnik@hotmail.ocm>
2025-05-16 23:28:44 +0200

    Rules

--- pages/rules.py
+++ pages/rules.py
@@ -3,10 +3,18 @@ import streamlit as st
 st.title("Moltress - Rules")
 
 
-with st.expander("Settling During the Game"):
-    st.write('''
-        1 - Governors need to settle new villages inside the kingdom borders. The only exception are capitols - croppers (9c and 15c or 7cc/6cc with great oasis crop bonus). 
-        2 - Reserving a new settling location can only be done when settlers have been trained and you have enough CP. 
-        3 - Governors must not settle on locations reserved for robber camps or treasuries, which will be marked on the map (RED). Settling on one of these tiles will result in the village being zeroed. 
-    ''')
-    st.image("https://static.streamlit.io/examples/dice.jpg")
+
+st.subheader("1. Settling During the Game")
+st.text(
+    "1.1 - Governors need to settle new villages inside the kingdom borders."
+    " The only exception are capitols - croppers (9c and 15c or 7cc/6cc with great oasis crop bonus). "
+)
+st.text(
+    "1.2 - Reserving a new settling location can only be done when settlers have been trained"
+    " and you have."
+)
+st.markdown(
+    "1.3 - Governors must not settle on locations reserved for robber camps or treasuries, "
+    "which will be marked on the map :red[(RED)]. Settling on one of these tiles will result in the "
+    "village being zeroed. "
+)
