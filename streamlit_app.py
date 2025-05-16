d21890698e682936a28fb00d78c9c0d2c449bb16
Damian <d.chalupnik@hotmail.ocm>
2025-05-16 22:52:42 +0200

    test navbar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from st_pages import add_page_title, get_nav_from_toml
 
-pg = st.navigation(["pages/rules.py", "pages/op.py"])
+st.set_page_config(layout="wide")
+nav = get_nav_from_toml(".streamlit/pages.toml")
+pg = st.navigation(nav)
+# pg = st.navigation(nav)
+#
+# add_page_title(pg)
+#
+# pg.run()
+# pg = st.navigation(["pages/rules.py", "pages/op.py"])
 pg.run()
