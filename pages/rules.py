d551e52e3f706d3fe3e55098a23adfe64a556e81
Damian <d.chalupnik@hotmail.ocm>
2025-05-16 23:30:53 +0200

    Rules

--- pages/rules.py
+++ pages/rules.py
@@ -6,9 +6,9 @@ st.title("Moltress - Rules")
 st.subheader("1. Settling During the Game")
 st.markdown(
     "1.1 - Governors need to settle new villages inside the kingdom borders."
-    " The only exception are capitols - croppers (9c and 15c or 7cc/6cc with great oasis crop bonus).<br/>"
+    " The only exception are capitols - croppers (9c and 15c or 7cc/6cc with great oasis crop bonus).<br>"
     "1.2 - Reserving a new settling location can only be done when settlers have been trained"
-    " and you have. <br/>"
+    " and you have. <br>"
     "1.3 - Governors must not settle on locations reserved for robber camps or treasuries, "
     "which will be marked on the map :red[(RED)]. Settling on one of these tiles will result in the "
     "village being zeroed. "
