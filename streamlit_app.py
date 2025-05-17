94c0375d7c2f13476298faa171a1d15d9b299801
Damian <d.chalupnik@hotmail.ocm>
2025-05-17 20:40:53 +0200

    next page

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,6 @@ import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
 
 
-# st.set_page_config(
-#     page_title="Moltress",
-#     page_icon="favicon.ico",
-#     layout="wide",
-# )
 st.logo("logo.png")
 st.set_page_config(layout="wide")
 nav = get_nav_from_toml(".streamlit/pages.toml")
@@ -14,7 +9,4 @@ pg = st.navigation(nav)
 pg = st.navigation(nav)
 
 add_page_title(pg)
-#
-# pg.run()
-# pg = st.navigation(["pages/rulesa.py", "pages/op.py"])
 pg.run()
