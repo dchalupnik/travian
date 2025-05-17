2c318d2ebdfc8667b3d4ff72eac4919b764d38dd
Damian <d.chalupnik@hotmail.ocm>
2025-05-17 20:24:05 +0200

    Rules

--- pages/rules.py
+++ pages/rules.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Moltress - Rules[Draft]")
 
 
-st.subheader("1. General rules")
+st.markdown("**1. :exclamation:General rules**")
 st.markdown('''
     1.1 - Everybody needs to be in secret society and on GT. Discord is optional.     
     1.2 - There is troop requirement 100% troops to population for romans 60%.  
@@ -11,21 +11,21 @@ st.markdown('''
 '''
 )
 
-st.subheader("2. Settling During the Game")
+st.markdown("**2. :house_buildings:Settling During the Game**")
 st.markdown('''
-    2.1 - *Governors need to settle new villages inside the kingdom borders*.
+    2.1 - **Governors need to settle new villages inside the kingdom borders**.
           The only exception are capitols - croppers (9c and 15c or 7cc/6cc with great oasis crop bonus).
           Borders ar marked with blue spots. You can settle on them.  
-    2.2 - Reserving a new settling location can only be done when *settlers have been trained and you have enough CP*.  
+    2.2 - Reserving a new settling location can only be done when **settlers have been trained and you have enough CP**.  
     2.3 - Governors must not settle on locations reserved for robber camps or treasuries, which will be marked
           on the map :red[RED]. Settling on one of these tiles will result in the village being zeroed.  
     2.4 - Player that dont have crop has priority to get one.  
     2.5 - There is possibility to reserve village to chief. Inactive players and non kingdom members allowed.    
 ''')
 
-st.subheader("3. Robber Camps")
+st.markdown("**3. :pirate_flag:Robber Camps**")
 st.markdown('''
-    3.1 - *Each player can attack up to 2 robber camps.*       
+    3.1 - **Each player can attack up to 2 robber camps.**       
     3.2 - Two hours after the spawning of camps, you are allow to 2 more camps if any are still there.  
     3.3 - The last player to send to a camp must mark it as 'Full' on the map for the rest of the kingdom to see.
     3.4 - Player that sent attack on camp should wrote on camps thread. Kingdom attacks are not visible on phone.    
@@ -33,18 +33,12 @@ st.markdown('''
 '''
 )
 
-st.subheader("4. Oasis Rules ")
+st.markdown("**4. :desert_island:Oasis Rules**")
 st.markdown('''
-    1.1 - *The max number of units in oasis is 250*. Only Cropper Capitals and Treasuries can be excluded from this 
+    1.1 - **The max number of units in oasis is 250**. Only Cropper Capitals and Treasuries can be excluded from this 
     rule.  
     1.2 -  Generally, priority is given to those that benefit the most from the Oasis. As an example, here is a typical 
     priority list on oasis priorities:  
-        - 15c Capital Treasury  
-        - 15c Capital  
-        - 9c Capital Treasury  
-        - 9c Capital  
-        - 7c Capital Treasury  
-        - 7c Capital  
-        - 6c Capital  
+    15c Capital Treasury > 15c Capital > 9c Capital Treasury > 9c Capital > 7c Capital Treasury > 7c Capital > 6c Capital  
 '''
 )
